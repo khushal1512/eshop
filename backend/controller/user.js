64eca3923c4051a35db4a2d1e6900e4173c9be71
khushal1512 <khushal080424@gmail.com>
2024-10-08 11:35:33 +0530

    nodemailer

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -3,6 +3,9 @@ const path = require("path");
 const router = express.Router();
 const { upload } = require("../multer");
 const User = require("../model/user");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
 
 // Route to create a new user
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
@@ -14,6 +17,16 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     const userEmail = await User.findOne({ email }); // Make sure to use `findOne` instead of `FindOne`
 
     if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        } else {
+          res.json({ message: "File deleted successfully" });
+        }
+      });
       return next(new ErrorHandler("User already exists", 400));
     }
 
@@ -28,6 +41,19 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     });
 
     await user.save();
+
+    const activationToken = createActivationToken(user);
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+
+    try  {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account",
+        message: `Hello ${user.name}, please click on the link to activate to your account: ${activationUrl}`
+      })
+    } catch(err) {
+      return next((new ErrorHandler(err.message, 500)));
+    }
     console.log(user);
     res.status(201).json({
       message: "User created successfully",
@@ -42,5 +68,12 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
     next(error); // Pass error to error handler
   }
 });
+
+// activation for email
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_TOKEN, {
+    eexpiresIn: "5m",
+  });
+};
 
 module.exports = router;
