118746525eb0a785b4f2e9dc9a0a4a224ccff3db
khushal1512 <khushal080424@gmail.com>
2024-10-10 10:43:40 +0530

    login-route

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -4,7 +4,6 @@ import styles from "../../styles/styles";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { server } from "../../server";
-import res from "express/lib/response";
 import { toast } from "react-toastify";
 
 const Login = () => {
