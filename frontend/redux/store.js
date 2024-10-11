bc4381e6df9ca0919f8c740cc0ade33a119e157d
khushal1512 <khushal080424@gmail.com>
2024-10-11 15:04:48 +0530

    redux-state-management

---
+++ frontend/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const Store = configureStore({
+  reducers: {},
+});
+
+export default Store;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {LoginPage, SignupPage, ActivationPage} from "./Routes.js";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
 const App = () => {
+  useEffect(() => {
+    axios
+      .get(`${server}/user/getuser`, { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/sign-up' element={<SignupPage/>} />
-        <Route path='/activation/:activation_token' element={<ActivationPage/>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
