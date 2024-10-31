f0df0fdec69950cbae184f3639334cb2e2b2f6f4
khushal1512 <khushal080424@gmail.com>
2024-10-31 23:09:11 +0530

    refactor-redux-reducers

--- frontend/src/redux/reducers/user.js
+++ frontend/src/redux/reducers/user.js
@@ -2,23 +2,29 @@ import { createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
   isAuthenticated: false,
+  loading: false,
+  user: null,
+  error: null,
 };
 
-export const userReducer = createReducer(initialState, {
-  LoadUserRequest: (state) => {
-    state.loading = true;
-  },
-  LoadUserSuccess: (state, action) => {
-    state.isAuthenticated = true;
-    state.loading = false;
-    state.user = action.payload;
-  },
-  LoadUserFail: (state, action) => {
-    state.loading = false;
-    state.error = action.payload;
-    state.isAuthenticated = false;
-  },
-  clearError: (state) => {
-    state.error = null;
-  },
+const userReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase('LoadUserRequest', (state) => {
+      state.loading = true;
+    })
+    .addCase('LoadUserSuccess', (state, action) => {
+      state.isAuthenticated = true;
+      state.loading = false;
+      state.user = action.payload;
+    })
+    .addCase('LoadUserFail', (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+      state.isAuthenticated = false;
+    })
+    .addCase('clearError', (state) => {
+      state.error = null;
+    });
 });
+
+export default userReducer;
